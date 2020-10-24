10b553ad060a617cbabb5e24033761837d0baf34
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-10-23 20:09:50 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = require("./controllers/burgers_controller.js");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(methodOverride("_method"));
 app.use(bodyParser.json());
